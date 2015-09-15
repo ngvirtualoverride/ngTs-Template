5bcd06f0aa3618607b0ff5e8e328ebdc43cacdc5
Arjay Elbore <virtualoverride@outlook.com>
2015-09-15 16:06:15 +0800

    add ts files

--- gulpfile.js
+++ gulpfile.js
@@ -15,21 +15,6 @@ var gulp = require('gulp'),
 var config = new Config();
 
 /**
- * Generates the app.d.ts references file dynamically from all application *.ts files.
- */
-// gulp.task('gen-ts-refs', function () {
-//     var target = gulp.src(config.appTypeScriptReferences);
-//     var sources = gulp.src([config.allTypeScript], {read: false});
-//     return target.pipe(inject(sources, {
-//         starttag: '//{',
-//         endtag: '//}',
-//         transform: function (filepath) {
-//             return '/// <reference path="../..' + filepath + '" />';
-//         }
-//     })).pipe(gulp.dest(config.typings));
-// });
-
-/**
  * Lint all custom TypeScript files.
  */
 gulp.task('ts-lint', function () {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,38 @@
 ((): void => {
 
-    var app = angular.module('demoApp', ['ngRoute', 'ngAnimate']);
+    // var app = angular.module('demoApp', ['ngRoute', 'ngAnimate']);
 
-    app.config(['$routeProvider', ($routeProvider) => {
-        $routeProvider.when('/',
-        {
-            controller: 'demoApp.CustomersController',
-            templateUrl: 'app/views/customers.html',
-            controllerAs: 'vm'
-        })
-        .when('/orders/:customerId',
-        {
-            controller: 'demoApp.OrdersController',
-            templateUrl: 'app/views/orders.html',
-            controllerAs: 'vm'
-        });
-    }]);
+    // app.config(['$routeProvider', ($routeProvider) => {
+    //     $routeProvider.when('/',
+    //     {
+    //         controller: 'demoApp.CustomersController',
+    //         templateUrl: 'app/views/customers.html',
+    //         controllerAs: 'vm'
+    //     })
+    //     .when('/orders/:customerId',
+    //     {
+    //         controller: 'demoApp.OrdersController',
+    //         templateUrl: 'app/views/orders.html',
+    //         controllerAs: 'vm'
+    //     });
+    // }]);
+
+    angular
+        .module('demoApp', ['ui.router', 'ngAnimate'])
+        .config(['$stateProvider', ($stateProvider) => {
+            $stateProvider
+                .state('customers', {
+                    url: '/',
+                    templateUrl: 'app/views/customers.html',
+                    controller: 'demoApp.CustomersController',
+                    controllerAs: 'vm'                         
+                })
+                .state('orders', {
+                    url: '/orders/:customerId',
+                    templateUrl: 'app/views/orders.html',
+                    controller: 'demoApp.OrdersController',
+                    controllerAs: 'vm'
+                });
+        }]);
 
 })();
